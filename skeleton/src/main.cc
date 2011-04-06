c29c868a9e85b33e26190971415d60bfe0d58458
Nicolas Vion <nico@yojik.eu>
2011-04-06 17:27:36 +0200

    Vieux code de test

--- skeleton/src/main.cc
+++
@@ -1,9 +0,0 @@
-
-#include <iostream>
-#include "skeleton.hh"
-
-int main(int argc, char *argv[]) {
-	Server server;
-	if (server.open((argc == 3) ? argv[1] : "0.0.0.0", (argc == 3) ? argv[2] : "4242"))
-		server.go();
-}
